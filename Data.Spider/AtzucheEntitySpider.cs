0d4f1f691d676231dfcb06e328e58bfe99573c6d
egman <egman@qq.com>
2021-12-29 18:10:55 +0800

    修改目录名称

--- Data.Spider/AtzucheEntitySpider.cs
+++ Data.Spider/AtzucheEntitySpider.cs
@@ -12,7 +12,7 @@ namespace Data.Spider
     {
         protected override void MyInit(params string[] arguments)
         {
-            AddPipeline(new SqlServerEntityPipeline("Server=.;Database=AuzucheSpider;uid=sa;pwd=123;MultipleActiveResultSets=true"));//注意连接字符串中数据库不能带 .  亲测报错。。。
+            AddPipeline(new SqlServerEntityPipeline("Server=.\\sql2019;Database=AuzucheSpider;Trusted_Connection=True;MultipleActiveResultSets=true"));//注意连接字符串中数据库不能带 .  亲测报错。。。
             AddStartUrl("http://www.atzuche.com/car/searchListMap/2?cityCode=330100&sceneCode=U002&filterCondition%5Blon%5D=120.219294&filterCondition%5Blat%5D=30.259258&filterCondition%5Bseq%5D=4&pageNum=1&pageSize=0");
             AddEntityType<AtzucheModel>();//如添加此实体类，框架将会根据此实体类上面的特性选择进行匹配，匹配成功后插入数据库，固可以省略Processor和Pipe，或者不使用此句，通过控制器和回调自定义存储方法
         }
